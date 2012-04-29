35611632e78f63a840e8d8b5c398390400e5128b
goG <rgogulakrishnan@gmail.com>
2012-04-29 11:14:48 +0530

    Added options to edit and showed all comments

--- discussion/views/admin/list_topics.php
+++ discussion/views/admin/list_topics.php
@@ -10,6 +10,7 @@
 			<th><?php echo lang('topic.topic_title_label'); ?></th>
 			<th class="collapse"><?php echo lang('topic.topic_author_label'); ?></th>
 			<th class="collapse"><?php echo lang('topic.topic_created_label'); ?></th>
+			<th class="collapse"><?php echo lang('topic.comments_label'); ?></th>
 			<th class="collapse"><?php echo lang('topic.topic_updated_label'); ?></th>
 			<th width="120"></th>
 		  </tr>
@@ -20,9 +21,20 @@
 				<td><?php echo $topic->title; ?></td>
 				<td><?php echo anchor('user/' . $topic->created_by, $topic->display_name, 'target="_blank"'); ?></td>
 				<td><?php echo format_date($topic->created_on); ?></td>
-				<td><?php echo format_date($topic->last_updated); ?></td>
-				<td><?php echo anchor('admin/discussion/view/' . $topic->id, lang('global:view'), array('class'=>'btn green')); ?>
-				<?php echo anchor('admin/discussion/delete/' . $topic->id, lang('global:delete'), array('class'=>'confirm btn red delete')); ?></td>
+				<td><?php echo $topic->tot_comments; ?></td>
+				<td>
+					<?php 
+						if(floor(now() - $topic->created_on/604800) > 0) { 
+							echo timespan($topic->created_on)." ago";
+						} else { 
+							echo format_date($topic->created_on);
+						} 
+					?> 
+				</td>
+				<td>
+					<?php echo anchor('admin/discussion/view/' . $topic->id, lang('global:view'), array('class'=>'btn green')); ?>
+					<?php echo anchor('admin/discussion/delete/' . $topic->id, lang('global:delete'), array('class'=>'confirm btn red delete')); ?>
+				</td>
 			</tr>
 			<?php endforeach; ?>
 		</tbody>
