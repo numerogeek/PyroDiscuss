29940115a1b76ae8795d1e022f861c35d3319347
goG <rgogulakrishnan@gmail.com>
2012-04-29 11:15:45 +0530

    lots of edits. Dont know what to comment!

--- discussion/views/admin/view_topic.php
+++ discussion/views/admin/view_topic.php
@@ -6,13 +6,27 @@
 
 	<div class="row">
 		<div class="span2">
+			<div class="group_title"><?php echo ucfirst($this->current_user->group); ?></div>
 			<p><?php echo gravatar($topic->user_email, 70);?></p>
-			<div><?php echo $topic->display_name; ?></div>
-			<div><?php echo format_date($topic->created_on); ?></div>
+			<div><strong><?php echo $topic->display_name; ?></strong></div>
+			<div class="date">
+				<?php 
+					if(floor(now() - $topic->created_on/604800) > 0) { 
+						echo timespan($topic->created_on)." ago";
+					} else { 
+						echo format_date($topic->created_on);
+					} 
+				?> 
+			</div>
 		</div>
 		<div class="span10">
 			<div class="topic_title"><?php echo $topic->title; ?></div>
-			<?php echo $topic->desc; ?>
+			<?php echo $topic->parsed; ?>
+			<?php if($this->current_user->id == $topic->created_by) { ?>
+				<div class="right">
+					<?php echo anchor('admin/discussion/edit/' . $topic->id, lang('global:edit'), array('class'=>'btn orange')); ?>
+				</div>
+			<?php } ?>
 		</div>
 		<hr>
 	</div>
@@ -26,17 +40,25 @@
 		<div class="span10">
 			<?php foreach($comments as $comment): ?>
 			
-				<div class="span7">
+				<div class="span6">
 					<?php echo $comment->comment; 
 					if($enable_delete) { ?>
 						<div class="top10 icon-remove"><?php echo anchor('admin/discussion/view/'.$topic->id.'/delete/'.$comment->id, lang('global:delete'), 'class="pad15"'); ?></div>
 					<?php } ?>
 				</div>
-				<div class="span2 comment_info">
+				<div class="span3 comment_info">
 					<div><?php echo gravatar($comment->user_email, 30);?></div>
 					<div>
-						<?php echo $comment->display_name; ?><br/>
-						<span class="date"><?php echo format_date($comment->created_on); ?></span>
+						<strong><?php echo $comment->display_name; ?></strong><br/>
+						<span class="date">
+						<?php 
+							if(floor(now() - $topic->created_on/604800) > 0) { 
+								echo timespan($topic->created_on)." ago";
+							} else { 
+								echo format_date($topic->created_on);
+							} 
+						?> 
+						</span>
 					</div>
 				</div>
 			
