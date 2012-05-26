8ed36af822d4739b597c98f1e531cb52d5ce916b
goG <rgogulakrishnan@gmail.com>
2012-05-27 01:48:07 +0530

    Get group name of users

---
+++ discussion/helpers/discussion_helper.php
@@ -0,0 +1,38 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Discussion Module
+*
+* @author		Gogula Krishnan Rajaprabhu
+* @package		Netpines
+* @subpackage	Discussion Module
+* @category	Helpers
+* @website		http://netpines.com
+*/
+
+/**
+* Return a users group name
+*
+* @param int $user the users id
+* @return  string
+*/
+function get_group_name($user_id)
+{
+	if (is_numeric($user_id))
+	{
+		$user = ci()->ion_auth->get_user($user_id);
+		
+		$user = (array) $user;
+		
+		if($user['group_id'])
+		{
+			$group = ci()->group_m->get_by('id', $user['group_id']);
+			
+			$group = (array) $group;
+			
+			return $group['description'];
+		
+		}
+	}
+	
+	return 'Member';
+}
